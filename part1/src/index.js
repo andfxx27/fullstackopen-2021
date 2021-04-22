8a4dcd04d51075052b84590f06c06e0f3d18848f
andfxx27 <idea.andersen@gmail.com>
2021-04-23 00:58:46 +0700

    feat: add part1, introduction to react component

---
+++ part1/src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Render React-component called "App" inside index.html, within div with id of "root"
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
