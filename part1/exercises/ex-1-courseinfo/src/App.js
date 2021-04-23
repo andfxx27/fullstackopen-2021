f9c24cd469ed15b95bf77c9342e69be6ec796609
andfxx27 <idea.andersen@gmail.com>
2021-04-23 21:29:32 +0700

    feat: add ex. courseinfo 1.1 and 1.2

--- part1/exercises/ex-1-courseinfo/src/App.js
+++ part1/exercises/ex-1-courseinfo/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+// Empty tag is fragment to avoid "extra" div-element
+const Header = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>
+      {props.course}
+    </h1>
   );
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.exerciseTitle} {props.exerciseCount}
+    </p>
+  )
 }
+
+// Content component contain three Part component
+const Content = (props) => {
+  return (
+    <div>
+      <Part exerciseTitle={props.part1} exerciseCount={props.exercise1} />
+      <Part exerciseTitle={props.part2} exerciseCount={props.exercise2} />
+      <Part exerciseTitle={props.part3} exerciseCount={props.exercise3} />
+    </div>
+  )
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises {props.part1ExerciseCount + props.part2ExerciseCount + props.part3ExerciseCount}
+    </p>
+  );
+};
+
+const App = () => {
+  // All data still resides in App component, and passed down to each child component via props
+  const course = 'Half stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercise1 = 10;
+  const part2 = 'Using props to pass data'
+  const exercise2 = 7;
+  const part3 = 'State of a component'
+  const exercise3 = 14;
+
+  return (
+    <>
+      <Header course={course} />
+      <Content part1={part1} exercise1={exercise1} part2={part2} exercise2={exercise2} part3={part3} exercise3={exercise3} />
+      <Total part1ExerciseCount={exercise1} part2ExerciseCount={exercise2} part3ExerciseCount={exercise3} />
+    </>
+  );
+};
 
 export default App;
