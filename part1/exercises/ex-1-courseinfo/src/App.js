d1812e2c29ae86ecea3a43384c7c9ec9bc61fe57
andfxx27 <idea.andersen@gmail.com>
2021-04-25 02:19:27 +0700

    refactor: adjust App.js

--- part1/exercises/ex-1-courseinfo/src/App.js
+++ part1/exercises/ex-1-courseinfo/src/App.js
@@ -4,7 +4,7 @@ import React from "react";
 const Header = (props) => {
   return (
     <h1>
-      {props.course}
+      {props.course.name}
     </h1>
   );
 };
@@ -21,36 +21,51 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part exerciseTitle={props.part1} exerciseCount={props.exercise1} />
-      <Part exerciseTitle={props.part2} exerciseCount={props.exercise2} />
-      <Part exerciseTitle={props.part3} exerciseCount={props.exercise3} />
+      <Part exerciseTitle={props.course.parts[0].name} exerciseCount={props.course.parts[0].exercises} />
+      <Part exerciseTitle={props.course.parts[1].name} exerciseCount={props.course.parts[1].exercises} />
+      <Part exerciseTitle={props.course.parts[2].name} exerciseCount={props.course.parts[2].exercises} />
     </div>
   )
 };
 
 const Total = (props) => {
+  let numberOfExercises = 0
+  props.course.parts.forEach(part => {
+    numberOfExercises += part.exercises
+  });
+
   return (
     <p>
-      Number of exercises {props.part1ExerciseCount + props.part2ExerciseCount + props.part3ExerciseCount}
+      Number of exercises {numberOfExercises}
     </p>
   );
 };
 
 const App = () => {
   // All data still resides in App component, and passed down to each child component via props
-  const course = 'Half stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercise1 = 10;
-  const part2 = 'Using props to pass data'
-  const exercise2 = 7;
-  const part3 = 'State of a component'
-  const exercise3 = 14;
+  const course = {
+    name: 'Half stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 15,
+      },     
+    ]
+  }
 
   return (
     <>
       <Header course={course} />
-      <Content part1={part1} exercise1={exercise1} part2={part2} exercise2={exercise2} part3={part3} exercise3={exercise3} />
-      <Total part1ExerciseCount={exercise1} part2ExerciseCount={exercise2} part3ExerciseCount={exercise3} />
+      <Content course={course} />
+      <Total course={course} />
     </>
   );
 };
