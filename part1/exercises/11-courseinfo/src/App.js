e3f7ab81be465af45b23de0958e91755fda150be
andfxx27 <idea.andersen@gmail.com>
2021-12-29 21:28:09 +0700

    feat: finished part1- b

--- part1/exercises/11-courseinfo/src/App.js
+++ part1/exercises/11-courseinfo/src/App.js
@@ -49,18 +49,20 @@ const Total = (props) => {
 
 // Root component
 const App = () => {
-  const course = "Half stack application development";
-  const parts = [
-    { title: "Fundamentals of React", exerciseCount: 10 },
-    { title: "Using props to pass data", exerciseCount: 7 },
-    { title: "State of a component", exerciseCount: 14 },
-  ];
+  const course = {
+    name: "Half stack application development",
+    parts: [
+      { title: "Fundamentals of React", exerciseCount: 10 },
+      { title: "Using props to pass data", exerciseCount: 7 },
+      { title: "State of a component", exerciseCount: 14 },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
--- part1/lessons/b-javascript/main.js
+++ part1/lessons/b-javascript/main.js
@@ -30,3 +30,78 @@ const arrayDestructure = () => {
   console.log(`second (prime[1]): ${second}`);
   console.log(`rest (prime[2:]):`, rest);
 };
+
+const objects = () => {
+  // Create object using object literal
+  let person = {
+    name: "Felix",
+    age: 21,
+    uid: 820283000,
+  };
+
+  console.log(person);
+
+  // Access single property with dot (.) or brackets ([])
+  console.log(person.name);
+  console.log(person["age"]);
+
+  // Add additional property by using dot or brackets
+  person.hobbies = ["game", "comic", "anime", "coding"];
+  person["university"] = "Binus University";
+
+  console.log("Final:", person);
+};
+
+const objectMethods = () => {
+  const arto = {
+    name: "Arto",
+    age: 35,
+    education: "PhD",
+    greet: function () {
+      // this refer to current object
+      console.log(`hello, my name is ${this.name}`);
+    },
+    doAddition: function (a, b) {
+      console.log(a + b);
+    },
+  };
+
+  arto.growOlder = function () {
+    this.age += 1;
+  };
+
+  arto.greet();
+
+  arto.doAddition(5, 6);
+
+  const referenceToDoAddition = arto.doAddition;
+  referenceToDoAddition(5, 7);
+
+  console.log(`${arto.name} is ${arto.age}`);
+  arto.growOlder();
+  console.log(`${arto.name} is ${arto.age}`);
+
+  // To preserver "this" when function is being referenced from outside the object
+  const referenceToGreet = arto.greet.bind(arto);
+
+  // this.name will be undefined
+  // Here, "this" is treated as a global object, not arto object
+  referenceToGreet();
+};
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    console.log(`Hello, my name is ${this.name}.`);
+  }
+}
+
+const adam = new Person("Adam Silvester", 23);
+adam.greet();
+
+const felix = new Person("Felix Andersen", 21);
+felix.greet();
