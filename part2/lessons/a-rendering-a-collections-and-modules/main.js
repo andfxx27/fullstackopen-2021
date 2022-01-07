810ef47fcda580628c36054fcd739cbaffeee0c3
andfxx27 <idea.andersen@gmail.com>
2022-01-07 15:40:39 +0700

    feat: finished p2a lesson

---
+++ part2/lessons/a-rendering-a-collections-and-modules/main.js
@@ -0,0 +1,14 @@
+const person = {
+  name: "felix",
+  age: 21,
+};
+
+// Will result in [Object object]
+console.log(
+  "Displaying object concatenated with string 'java's way': " + person
+);
+
+console.log(
+  "Displaying object with string as comma separated arguments: ",
+  person
+);
