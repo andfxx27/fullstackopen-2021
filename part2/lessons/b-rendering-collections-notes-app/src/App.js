d7d7009ba979fce2d52f04f45ac2ffd3fe75fb87
andfxx27 <idea.andersen@gmail.com>
2022-01-08 17:56:43 +0700

    feat: finished p2b-forms initial lesson

--- part2/lessons/b-rendering-collections-notes-app/src/App.js
+++ part2/lessons/b-rendering-collections-notes-app/src/App.js
@@ -3,10 +3,46 @@ import React, { useState } from "react";
 import "./App.css";
 import Note from "./components/Note";
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+
+  // To re-render a component, we must use the function returned by useState(...)
+  // Therefore, since we assign App's component state to input's value attribute-
+  //-it means App component now controll the behavior of the input element
+  // An onChange handler is needed to synchronize the typed value with component's state
+  const [newNote, setNewNote] = useState("");
+
+  const addNewNote = (event) => {
+    // Prevents default action of submitting form, one of which cause the page to reload
+    event.preventDefault();
+    const newNoteObject = {
+      id: notes.length + 1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+    };
+
+    setNotes(notes.concat(newNoteObject));
+    setNewNote("");
+  };
+
+  const handleNewNoteInputFormChange = (event) => {
+    setNewNote(event.target.value);
+  };
+
   return (
     <div>
       <h1>Notes</h1>
+      {/* Form for adding new note */}
+      <form onSubmit={addNewNote}>
+        <input
+          value={newNote}
+          onChange={handleNewNoteInputFormChange}
+          placeholder="add new note"
+        />
+        <button type="submit">Add</button>
+      </form>
+
       <ul>
         {notes.map((note) => {
           return <Note key={note.id} note={note} />;
